657a111034f975433badc9077317f28a55ea1da6
= <=>
2016-07-01 10:11:24 -0600

    Working font

--- zerospec/zerospec.ino
+++ zerospec/zerospec.ino
@@ -21,81 +21,98 @@ const int sdinPin = 11;  // DN(MOSI) - Serial data, pin 6 on LCD.
 const int sclkPin = 13;  // SCLK - Serial clock, pin 7 on LCD.
 const int blPin = 9;    // LED - Backlight LED, pin 8 on LCD.
 
-const char font[][18] PROGMEM = {
-  "5-0114afc620", // A
-  "5-07a3e8c7c0", // B
-  "5-03a30845c0", // C
-  "5-07a318c7c0", // D
-  "4-0f8e88f0", // E
-  "4-0f8e8880", //  F
-  "5-03a309c5c0", // G 
-  "5-0463f8c620", //  H
-  "1-7e", //  I
-  "3-049378", //  J
-  "5-04654e4a20", //  K
-  "4-088888f0", //  L
-  "7-01071d5ab56480", //  M
-  "5-047359c620", //  N
-  "6-01e861861780", // O 
-  "4-0f9f8880", //  P
-  "6-01c8a28a6781", //  Q
-  "5-07a31f4620", //  R
-  "5-03a28345c0", //  S
-  "5-07c8421080", //  T
-  "5-046318c5c0", //  U
-  "5-046318a880", //  V
-  "7-0126ad5aa89100", //  W
-  "5-0454422a20", //  X
-  "5-0463171080", //  Y
-  "5-07c26643e0", //  Z
-  "1-02", //
-  "1-02", //
-  "1-02", //
-  "1-02", //
-  "1-02", //
-  "1-02", //
-  "4-00797970", //  a
-  "4-088e99e0", //  b
-  "4-00698960", //  c
-  "4-01179970", //  d
-  "4-0069f870", //  e
-  "3-0d7490", //  f
-  "4-00797197", // g 
-  "4-088e9990", //  h
-  "1-5e", //  i
-  "2-1156", // j 
-  "4-089aca90", //  k
-  "1-7e", //  
-  "6-00083e965940", //  
-  "4-008f9990", //  
-  "4-00069960", //  
-  "4-00e99e88", //  
-  "4-00079711", //  
-  "3-027920", //  
-  "4-069c2970", //  
-  "3-017490", //  
-  "4-00099960", //  
-  "5-000118a880", //  
-  "6-000021b52480", //  
-  "4-00096690", //  
-  "4-00099626", //  
-  "4-00f248f0", //  
-  "4-06999960", //  
-  "3-079248", //  
-  "4-069168f0", //  
-  "4-06921960", //  
-  "4-0999f110", //  
-  "4-0f8e1960", //  
-  "4-068e9960", //  
-  "4-0f124440", //  
-  "4-06969960", //  
-  "4-06997160", //  
-  "1-02", //  
-  "4-06912020", //  
-  "1-7a", //  
-  "1-12", //  
-  "3-000e00", //  
- };
+const char font_exclaim[] PROGMEM = "1-7a";
+const char font_double_quote[] PROGMEM = "1-02";
+const char font_pound[] PROGMEM = "1-02";
+const char font_dollar[] PROGMEM = "1-02";
+const char font_percent[] PROGMEM = "1-02";
+const char font_amp[] PROGMEM = "1-02";
+const char font_quote[] PROGMEM = "1-02";
+const char font_lparen[] PROGMEM = "1-02";
+const char font_rparen[] PROGMEM = "1-02";
+const char font_star[] PROGMEM = "1-02";
+const char font_plus[] PROGMEM = "1-02";
+const char font_comma[] PROGMEM = "1-02";
+const char font_dash[] PROGMEM = "3-000e00";
+const char font_period[] PROGMEM = "1-02";
+const char font_fwd_slash[] PROGMEM = "1-02";
+const char font_0[] PROGMEM = "4-06999960";
+const char font_1[] PROGMEM = "3-079248";
+const char font_2[] PROGMEM = "4-069168f0";
+const char font_3[] PROGMEM = "4-06921960";
+const char font_4[] PROGMEM = "4-0999f110";
+const char font_5[] PROGMEM = "4-0f8e1960";
+const char font_6[] PROGMEM = "4-068e9960";
+const char font_7[] PROGMEM = "4-0f124440";
+const char font_8[] PROGMEM = "4-06969960";
+const char font_9[] PROGMEM = "4-06997160";
+const char font_colon[] PROGMEM = "1-12";
+const char font_semicolon[] PROGMEM = "";
+const char font_lt[] PROGMEM = "";
+const char font_equal[] PROGMEM = "";
+const char font_gt[] PROGMEM = "";
+const char font_question[] PROGMEM = "4-06912020";
+const char font_at[] PROGMEM = "";
+const char font_A[] PROGMEM = "5-0114afc620";
+const char font_B[] PROGMEM = "5-07a3e8c7c0";
+const char font_C[] PROGMEM = "5-03a30845c0";
+const char font_D[] PROGMEM = "5-07a318c7c0";
+const char font_E[] PROGMEM = "4-0f8e88f0";
+const char font_F[] PROGMEM = "4-0f8e8880";
+const char font_G[] PROGMEM = "5-03a309c5c0";
+const char font_H[] PROGMEM = "5-0463f8c620";
+const char font_I[] PROGMEM = "1-7e";
+const char font_J[] PROGMEM = "3-049378";
+const char font_K[] PROGMEM = "5-04654e4a20";
+const char font_L[] PROGMEM = "4-088888f0";
+const char font_M[] PROGMEM = "7-01071d5ab56480";
+const char font_N[] PROGMEM = "5-047359c620";
+const char font_O[] PROGMEM = "6-01e861861780";
+const char font_P[] PROGMEM = "4-0f9f8880";
+const char font_Q[] PROGMEM = "6-01c8a28a6781";
+const char font_R[] PROGMEM = "5-07a31f4620";
+const char font_S[] PROGMEM = "5-03a28345c0";
+const char font_T[] PROGMEM = "5-07c8421080";
+const char font_U[] PROGMEM = "5-046318c5c0";
+const char font_V[] PROGMEM = "5-046318a880";
+const char font_W[] PROGMEM = "7-0126ad5aa89100";
+const char font_X[] PROGMEM = "5-0454422a20";
+const char font_Y[] PROGMEM = "5-0463171080";
+const char font_Z[] PROGMEM = "5-07c26643e0";
+const char font_lbracket[] PROGMEM = "1-02";
+const char font_backslash[] PROGMEM = "1-02";
+const char font_rbracket[] PROGMEM = "1-02";
+const char font_caret[] PROGMEM = "1-02";
+const char font_underscore[] PROGMEM = "1-02";
+const char font_tilde[] PROGMEM = "1-02";
+const char font_a[] PROGMEM = "4-00797970";
+const char font_b[] PROGMEM = "4-088e99e0";
+const char font_c[] PROGMEM = "4-00698960";
+const char font_d[] PROGMEM = "4-01179970";
+const char font_e[] PROGMEM = "4-0069f870";
+const char font_f[] PROGMEM = "3-0d7490";
+const char font_g[] PROGMEM = "4-00797197";
+const char font_h[] PROGMEM = "4-088e9990";
+const char font_i[] PROGMEM = "1-5e";
+const char font_j[] PROGMEM = "2-1156";
+const char font_k[] PROGMEM = "4-089aca90";
+const char font_l[] PROGMEM = "1-7e";
+const char font_m[] PROGMEM = "6-00083e965940";
+const char font_n[] PROGMEM = "3-000d68";
+const char font_o[] PROGMEM = "4-00069960";
+const char font_p[] PROGMEM = "4-00e99e88";
+const char font_q[] PROGMEM = "4-00079711";
+const char font_r[] PROGMEM = "3-027920";
+const char font_s[] PROGMEM = "2-039c";
+const char font_t[] PROGMEM = "3-017490";
+const char font_u[] PROGMEM = "4-00099960";
+const char font_v[] PROGMEM = "5-000118a880";
+const char font_w[] PROGMEM = "6-000021b52480";
+const char font_x[] PROGMEM = "4-00096690";
+const char font_y[] PROGMEM = "4-00099626";
+const char font_z[] PROGMEM = "4-00f248f0";
+
+const char * const font_table[] PROGMEM = {font_exclaim, font_double_quote, font_pound, font_dollar, font_percent, font_amp, font_quote, font_lparen, font_rparen, font_star, font_plus, font_comma, font_dash, font_period, font_fwd_slash, font_0, font_1, font_2, font_3, font_4, font_5, font_6, font_7, font_8, font_9, font_colon, font_semicolon, font_lt, font_equal, font_gt, font_question, font_at, font_A, font_B, font_C, font_D, font_E, font_F, font_G, font_H, font_I, font_J, font_K, font_L, font_M, font_N, font_O, font_P, font_Q, font_R, font_S, font_T, font_U, font_V, font_W, font_X, font_Y, font_Z, font_lbracket, font_backslash, font_rbracket, font_caret, font_underscore, font_tilde, font_a, font_b, font_c, font_d, font_e, font_f, font_g, font_h, font_i, font_j, font_k, font_l, font_m, font_n, font_o, font_p, font_q, font_r, font_s, font_t, font_u, font_v, font_w, font_x, font_y, font_z};
 
 byte displayMap[LCD_WIDTH * LCD_HEIGHT / 8];
 const int MESSAGE_BUFFER_LEN = 64;
@@ -109,27 +126,30 @@ void setup() {
   clearDisplay(WHITE);
   updateDisplay();
 
-  strncpy(messageBuffer, "Hello World", MESSAGE_BUFFER_LEN);
+  strncpy(messageBuffer, "Hello World! This goes on to multiple lines.", MESSAGE_BUFFER_LEN);
   writeMessageBuffer();
 }
 
-int startX = 0;
-int startY = 0;
+int gTextStartX = 0;
+int gTextStartY = 0;
 
 void writeMessageBuffer(){  
   clearDisplay(WHITE);
-  int x = 0;
-  int y = 0;
+  gTextStartX = 0;
+  gTextStartY = 0;
 
   for(int i = 0; i < MESSAGE_BUFFER_LEN; i++){
+    if(gTextStartX > (LCD_WIDTH - 8)){
+      gTextStartX = 0;
+      gTextStartY += 8;
+    }
+
     if(messageBuffer[i] == 0){
       break;
     } else if(messageBuffer[i] == ' '){
-      x += 6;
+      gTextStartX += 6;
     } else {
-      startX = x;
-      startY = 20;
-      x += writeCharacter(messageBuffer[i]) + 2;
+      gTextStartX += writeCharacter(messageBuffer[i]) + 2;
     }
     updateDisplay();
   }
@@ -141,22 +161,22 @@ int writeCharacter(char character){
   bool charPixels[8 * 8];
   int bitPosn = 0;
 
-//  for(int i = 0; i < 8 * 8; i++){
-//    charPixels[i] = false;
-//  }
+  int fontIdx = character - 0x21; 
 
-  int fontIdx = character - 0x41;
-  for(int charIdx = 0; charIdx < strlen(font[fontIdx]); charIdx++){
+  char fontBuffer[16];
+  strcpy_P(fontBuffer, (char*)pgm_read_word(&(font_table[fontIdx])));   
+   
+  for(int charIdx = 0; charIdx < strlen(fontBuffer); charIdx++){
     char byteBuff[2];
 
     // Determine the width of the letter
     if(charIdx == 0){
     
       char w[1];
-      w[0] = font[fontIdx][charIdx];
+      w[0] = fontBuffer[charIdx];
       charWidth = atoi(w); 
     } else if(charIdx >= 2){
-      byteBuff[charIdx % 2] = font[fontIdx][charIdx];
+      byteBuff[charIdx % 2] = fontBuffer[charIdx];
       if(charIdx % 2 == 1){
         unsigned long hex = strtoul(byteBuff, NULL, 16);
         for(int bit = 0; bit < 8; bit++){
@@ -172,14 +192,16 @@ int writeCharacter(char character){
   for(int x = 0; x < charWidth; x++){
     for(int y = 0; y < 8; y++){
       if(charPixels[x + y * charWidth]){
-        setPixel(x + startX, y + startY);  
+        setPixel(x + gTextStartX, y + gTextStartY);  
       } else {
-        clearPixel(x + startX, y + startY);
+        clearPixel(x + gTextStartX, y + gTextStartY);
       }      
     }
   }
   return charWidth;
 }
+
+
 
 void loop() {
   // put your main code here, to run repeatedly:
